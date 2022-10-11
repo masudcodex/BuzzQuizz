aab08c40428a04f3e2364b7e0db498eb5842bb39
masudcodex <forester.masud@gmail.com>
2022-10-11 12:06:35 +0600

    Hero banner completed

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import { Col, Container, Row } from 'react-bootstrap';
+import './Home.css';
 
 const Home = () => {
     return (
         <div>
-          <h2>Home</h2>  
+            <div className="hero-banner">
+                <Container>
+                    <Row>
+                        <Col align="start" className='py-5'>
+                            <h2 className='pb-3'>Best Online Quizzes</h2>
+                            <p>Keeping yourself entertained and educated is just a quiz away. Want to have some fun or learn about a topic? Curated to learn while having fun, our online quizzes are an excellent source of knowledge and entertainment. Take a quiz today to discover amazing facts about yourself or the world.</p>
+                        </Col>
+                        <Col></Col>
+                    </Row>
+                </Container> 
+            </div>
         </div>
     );
 };
