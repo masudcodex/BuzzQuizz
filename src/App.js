dc60cf8dc6b8054e7ae835f1197ce7dd440a0009
masudcodex <forester.masud@gmail.com>
2022-10-12 16:12:42 +0600

    statistics page completed

--- src/App.js
+++ src/App.js
@@ -26,6 +26,7 @@ function App() {
         },
         {
           path: '/statistics',
+          loader: async ()=> fetch('https://openapi.programming-hero.com/api/quiz'),
           element: <Statistics></Statistics>
         },
         {
--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -1,9 +1,31 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
+import { useLoaderData } from 'react-router-dom';
+import { PieChart, Pie, Legend, Tooltip, ResponsiveContainer, Cell } from 'recharts';
+import './Statistics.css'
 
 const Statistics = () => {
+    const quizzes = useLoaderData().data;
+    const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
+
+    
+
     return (
         <div>
-            <h2>Statistics</h2>
+            <div className='page-header'>
+                <h2>Quiz Statistics</h2>
+                <p>Number of quizzes per topics</p>
+            </div>
+            <div className='statisticalChart'>
+                <PieChart width={400} height={400}>
+                    <Pie dataKey="total" data={quizzes} fill="#8884d8" label>
+                    {quizzes.map((entry, index) => (
+                    <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+                    ))}
+                    </Pie>
+                    <Tooltip />
+                    <Legend />
+                </PieChart>
+            </div>
         </div>
     );
 };
